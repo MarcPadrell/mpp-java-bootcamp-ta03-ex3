ad8716b8a648ddc90a34f27b12141a0de378b4bc
MarcPadrell <marc.padrell@hotmail.com>
2022-04-05 23:45:35 +0200

    File .java committed.

---
+++ Ex03/src/ex03/ComentarioVariasLineas.java
@@ -0,0 +1,15 @@
+package ex03;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+		for (int i = 0; i<6;/*Ahora viene el paso*/ i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DEL PROGRAMA");
+
+	}
+
+}
